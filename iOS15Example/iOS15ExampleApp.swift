924946a776102935f112dc299d57d3f54dad07b3
shi-n <shi-n_xyz@cloudsquare.jp>
2021-06-09 06:40:12 +0900

    Initial Commit

---
+++ iOS15Example/iOS15ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS15ExampleApp.swift
+//  iOS15Example
+//
+//  Created by Shin Inaba on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct iOS15ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
